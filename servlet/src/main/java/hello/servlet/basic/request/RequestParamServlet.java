a86258776308bd6220a125514ea4cb7d1557c218
aammddkkzxc <ideal0427@naver.com>
2024-08-11 04:34:45 +0900

    feat: HTTP 요청 데이터 - GET 쿼리 파라미터 HttpServletRequest 가 제공하는 다음 메서드를 통해 쿼리 파라미터를 편리하게 조회 getParameter getParameterNames getParameterValues

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,39 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("전체 파라미터 조회 - start");
+        Enumeration<String> parameterNames = req.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String paramName = parameterNames.nextElement();
+            System.out.println(paramName + "=" + req.getParameter(paramName));
+        }
+        req.getParameterNames().asIterator().forEachRemaining(name -> System.out.println(name + "= " + req.getParameter(name)));
+        System.out.println("전체 파라미터 조회 - end");
+
+        System.out.println("단일 파라미터 조회");
+        String userName = req.getParameter("userName");
+        System.out.println("request.getParameter(userName) = " + userName);
+        String age = req.getParameter("age");
+        System.out.println("request.getParameter(age) = " + age);
+
+        System.out.println("이름이 같은 복수 파라미터 조회");
+        System.out.println("request.getParameterValues(userName)");
+        String[] userNames = req.getParameterValues("userName");
+        for (String name : userNames) {
+            System.out.println("userName=" + name); }
+
+        resp.getWriter().write("ok");
+    }
+}
